f87f369a1ff47703e392df32db986f4100714860
mac <mac@macs-MacBook-Air.local>
2026-09-18 19:50:56 +0530

    feat(analytics): install Google tag (gtag.js AW-825590065) in root layout head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -172,6 +172,21 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-825590065"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-825590065');
+            `
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdData) }}
